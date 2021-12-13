7ccf82eab105ad7f382e211dd5e78efcd8883b60
Boshra-01 <boshra.cse@gmail.com>
2021-12-13 21:27:25 +0600

    call

---
+++ app/src/main/java/com/example/phonecall/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.phonecall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button call;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        call = (Button) findViewById(R.id.button_call);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:123456789"));
+
+
+                startActivity(intent);
+            }
+        });
+    }
+}
